c41ef2c6e6cd4e516b5fdc9ef3738775cb7d6a26
zBeppe123 <72344861+zBeppe123@users.noreply.github.com>
2020-10-24 23:04:53 +0200

    Menu ver 0.1
    an embrional version of the menu (need to adjust the button size and add other code when we will need to do the save data on DB)

---
+++ Fantasy Riot/Assets/MenuScript/LogInMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogInMenu : MonoBehaviour
+{
+    public InputField insertname;                 //User name
+    public InputField insertpassword;         //user password
+
+    public Button submitButton;             //Submit button
+
+    public void CallLogin()
+    {
+        StartCoroutine(LoginPlayer());
+    }
+    IEnumerator LoginPlayer()                       //this function Login the account in the DB
+    {
+        //WWWForm create an object "form" that can be used to interact whit DB
+        WWWForm form = new WWWForm();
+        form.AddField("name", insertname.text);                               //adding the name field
+        form.AddField("password", insertpassword.text);                           //adding the password field
+        WWW www = new WWW("http://localhost/sqlconnect/login.php", form);     //WWW create a connection with the page wher the php page is
+        yield return www;                                               //return the www info page but continue the program
+        if (www.text[0] == '0')                                        //if the contact with the page was succesfull
+        {
+            DBManager.username = insertname.text;                    //nel file DVManager insert the username and the score
+            DBManager.score = int.Parse(www.text.Split('\t')[1]);
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
+        else //if it was not
+        {
+            Debug.Log("User login failed. Error #" + www.text); // show the error in the debug
+        }
+    }
+    public void VerifyInputs() //check if the information are accceptable
+    {
+        submitButton.interactable = (insertname.text.Length >= 8 && insertpassword.text.Length >= 8);
+    }
+}
---
+++ Fantasy Riot/Assets/MenuScript/RegisterMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RegisterMenu : MonoBehaviour
+{
+    public InputField insertname;                 //User name
+    public InputField insertpassword;         //user password
+    
+    public Button submitButton;             //Submit button
+
+    public void CallRegister()              //function that call the function Register
+    {
+        StartCoroutine(Register());              //StarCoroutine make sure that the user cant change page when the botton is clicked
+        //CallLogin();
+    }
+    IEnumerator Register()                       //this function Register the account in the DB
+    {
+        //WWWForm create an object "form" that can be used to interact whit DB
+        WWWForm form = new WWWForm();
+        form.AddField("name", insertname.text);                               //adding the name field
+        form.AddField("password", insertpassword.text);                           //adding the password field
+        WWW www = new WWW("http://localhost/sqlconnect/register.php",form);                      //WWW create a connection with the page wher the php page is
+        yield return www;                                               //return the www info page but continue the program
+        if (www.text == "0")                                        //if the contact with the page was succesfull
+        {
+            Debug.Log("User created.");
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
+        else //if it was not
+        {
+            Debug.Log("User cration failed. Error #" + www.text); // show the error in the debug
+        }
+
+    }
+    public void VerifyInputs() //check if the information are accceptable
+    {
+        submitButton.interactable = (insertname.text.Length >= 8 && insertpassword.text.Length >= 8);
+    }
+    
+}

36373ae6a92085172225d03f1ab92ca3ceb71dc0
zBeppe123 <72344861+zBeppe123@users.noreply.github.com>
2020-10-29 23:56:22 +0100

    Revert "Revert "Merge branch 'main' of https://github.com/Reyleen/FantasyRiot into main""
    This reverts commit 985c8d95194a1f5c246f45a27198e80d67840f73.

---
+++ Fantasy Riot/Assets/Scripts/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Slider healthBar;
+    public Text HPText;
+    public PlayerHealthManager playerHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.maxValue = playerHealth.playerMaxHealth;
+        healthBar.value = playerHealth.playerCurrentHealth;
+        HPText.text = "HP: " + playerHealth.playerCurrentHealth + "/" + playerHealth.playerMaxHealth;
+    }
+}

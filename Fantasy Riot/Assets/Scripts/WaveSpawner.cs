6896e81f0369a41152685bd622648183a77f7aa3
Frank KH <FrankGaudin@users.noreply.github.com>
2020-11-08 21:20:42 +0100

    first wave spawner

---
+++ Fantasy Riot/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{ 
+    public enum SpawnState (SPAWNING, WAITING, COUNTING);
+
+    [System Serializable]
+    public class Wave
+    {
+        public string name;
+        public Transform enemy;
+        public int count;
+        public float rate;
+    }
+
+    public Wave[] waves;
+    private int nextWave = 0;
+
+    public Transform[] spawnPoints;
+
+    public float timeBetweenWaves = 10f;
+    private float waveCountdown;
+
+    private float searchCountdown = 1f;
+
+    private SpawnState state = SpawnState.COUNTING;
+
+    void Start()
+    {
+        waveCountdown = timeBetweenWaves;
+    }
+
+    void Update()
+    {
+        if (state == SpawnState.WAITING)
+        {
+            if (!EnemyIsAlive())
+            {
+                return;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        if(waveCountdown <= 0)
+        {
+            if(state != SpawnState.SPAWNING)
+            {
+                startCoroutine(SpawnWave(waves[nextWave]));
+            } 
+    } else
+        {
+            waveCountdown == timeBetweenWaves.deltaTime;
+        }
+    }
+
+    void WaveCompleted()
+    {
+        state = SpawnState.COUNTING;
+        waveCountdown = timeBetweenWaves;
+
+        if(nextWave + 1 > waves.Length -1)
+        {
+            nextWave = 0;
+        } else
+        {
+            nextWave++;
+        }
+
+    }
+
+    bool EnemyIsAlive()
+    {
+        searchCountdown -= Time.deltaTime;
+        if (searchCountdown <= 0f)
+        {
+            if (GameObject.FindGameObjectWithTag("Enemy") == null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    IEnumerator SpawnWave(Wave _wave)
+    {
+        state = SpawnState.SPAWNING;
+
+        for(int i = 0; i< _wave.count; i++)
+        {
+            SpawnEnemy(_wave.enemy);
+            yield return new WaitForSeconds(1f / _wave.rate);
+        }
+
+        state = SpawnState.WAITING;
+
+        yield break;
+    }
+
+    void SpawnEnemy (Transform _enemy)
+    {
+        Transform _sp = spawnPoint[Random.Range(0, spawnPoints.length)];
+        Instantiate(_enemy, _sp.transform.position, _sp.transform.rotation);
+    }
+}

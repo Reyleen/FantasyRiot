38a340ff082d34dc79eb8218a2ab0bfe0d169f44
Frank KH <FrankGaudin@users.noreply.github.com>
2020-11-08 16:32:30 +0100

    Now joysticks disappear when opening the pause menu

--- Fantasy Riot/Assets/Scripts/PauseMenu.cs
+++ Fantasy Riot/Assets/Scripts/PauseMenu.cs
@@ -4,7 +4,7 @@ using System.Threading;
 
 public class PauseMenu : MonoBehaviour
 {
-    public GameObject pauseMenuUI, PauseButton;
+    public GameObject pauseMenuUI, PauseButton, MoveJoystick, AimJoystik;
 
 
     public void Resume()
@@ -12,6 +12,8 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         PauseButton.SetActive(true);
+        MoveJoystick.SetActive(true);
+        AimJoystik.SetActive(true);
     }
 
     public void Pause ()
@@ -19,5 +21,7 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         PauseButton.SetActive(false);
+        MoveJoystick.SetActive(false);
+        AimJoystik.SetActive(false);
     }
 }

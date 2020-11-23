7272a6beab0a123bfa51f543261c1630c0d4efb0
zBeppe123 <72344861+zBeppe123@users.noreply.github.com>
2020-11-23 13:23:34 +0100

    Adjusted Menu + intro
    added random object animation in play menu
    modified button
    added an Intro for the game (using timeline)
    changed character in selection screen
    added loading scene (can be used in other scene change)
    modified canvas for phone support
     Note: Text mash pro don't matain object after commit. we need to modifie them again after. Music is not active in menu

---
+++ Fantasy Riot/Assets/MenuScript/DetectTouchIntro.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DetectTouchIntro : MonoBehaviour
+{
+    private GameObject wrap;
+    // Update is called once per frame
+    private void Start()
+    {
+        wrap = GameObject.Find("LevelLoader");
+    }
+    void Update()
+    {
+        if (Input.touchCount > 0) 
+        { 
+             Debug.Log("entrato");
+             wrap.GetComponent<LoadLevel>().LoadScreen("Menu");
+        }
+    }
+}

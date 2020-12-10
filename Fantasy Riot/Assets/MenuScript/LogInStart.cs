c1fda49ca537a4c131d9d74dcb779a335fe4fde7
zBeppe123 <72344861+zBeppe123@users.noreply.github.com>
2020-12-10 16:48:43 +0100

    AddedMenu Save Login

---
+++ Fantasy Riot/Assets/MenuScript/LogInStart.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogInStart : MonoBehaviour
+{
+    private Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = new Player();
+        player.LoadPlayer();
+        if(!string.IsNullOrEmpty(player.name))
+        StartCoroutine(LoginPlayer());
+    }
+    IEnumerator LoginPlayer()                       //this function Login the account in the DB
+    {
+        //WWWForm create an object "form" that can be used to interact whit DB
+        WWWForm form = new WWWForm();
+        form.AddField("name", player.name);                               //adding the name field
+        form.AddField("password", player.password);                           //adding the password field
+        WWW www = new WWW("http://localhost/sqlconnect/login.php", form);     //WWW create a connection with the page wher the php page is
+        Debug.Log("insert name =" + player.name);
+        Debug.Log("insert password =" + player.password);
+        yield return www;                                               //return the www info page but continue the program
+        Debug.Log("error =" + www.text[0]);
+        if (www.text[0] == '0')                                        //if the contact with the page was succesfull
+        {
+            DBManager.username = player.name;                    //in the file DVManager insert the username and the score
+            DBManager.score = int.Parse(www.text.Split('\t')[1]);
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
+        else //if it was not
+        {
+            Debug.Log("User login failed. Error #" + www.text); // show the error in the debug
+        }
+    }
+}
---
+++ Fantasy Riot/Assets/MenuScript/Webtest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Webtest : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        WWW request = new WWW("http://localhost/sqlconnect/webtest.php");
+        yield return request;
+        string[] webResult = request.text.Split('\t');
+        Debug.Log(webResult[0]);
+        int webNumer = int.Parse(webResult[1]);
+        webNumer *= 2;
+        Debug.Log(webNumer);
+    }
+
+}

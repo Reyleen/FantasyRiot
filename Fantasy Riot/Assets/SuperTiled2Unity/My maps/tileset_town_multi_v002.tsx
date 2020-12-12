069c80ff391392f11a736f87f2eb5123dfe15d89
Frank KH <FrankGaudin@users.noreply.github.com>
2020-12-12 19:26:18 +0100

    Added story map

---
+++ Fantasy Riot/Assets/SuperTiled2Unity/My maps/tileset_town_multi_v002.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="tileset_town_multi_v002" tilewidth="16" tileheight="16" tilecount="2304" columns="32">
+ <image source="tileset_town_multi_v002.png" width="512" height="1152"/>
+</tileset>

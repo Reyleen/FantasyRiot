83ed36437f9bcdca1efcb8915664c971aebdc992
Frank KH <72342418+FrankGaudin@users.noreply.github.com>
2021-01-08 11:51:13 +0100

    Intro scene now is more enjoyable

---
+++ Fantasy Riot/Assets/SuperTiled2Unity/My maps/Medievalhouses21.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Medievalhouses2" tilewidth="32" tileheight="32" tilecount="576" columns="24">
+ <image source="Medievalhouses2.png" width="768" height="768"/>
+</tileset>
---
+++ Fantasy Riot/Assets/SuperTiled2Unity/My maps/tileset_town_multi_v0021.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="tileset_town_multi_v002" tilewidth="32" tileheight="32" tilecount="576" columns="16">
+ <image source="tileset_town_multi_v002.png" width="512" height="1152"/>
+</tileset>

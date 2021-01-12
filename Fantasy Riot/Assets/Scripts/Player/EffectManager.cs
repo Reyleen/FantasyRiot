0878067a65560b409cf7e1f5c723336619cb2532
Frank KH <72342418+FrankGaudin@users.noreply.github.com>
2021-01-12 14:18:04 +0100

    Added sounds to dungeon

---
+++ Fantasy Riot/Assets/Scripts/Player/EffectManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectManager : MonoBehaviour
+{
+    public AudioSource step;
+    public PlayerMovement player;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.playerMoving)
+        {
+            if(!step.isPlaying)
+            step.Play();
+        } else
+        {
+            step.Stop();
+        }
+    }
+}

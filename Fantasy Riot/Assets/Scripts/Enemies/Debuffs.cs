9bf7bbf8d6f778087a896e81e2f629cacc2d1a7f
Reyleen <72262745+Reyleen@users.noreply.github.com>
2021-01-15 22:09:49 +0100

    Added Assassin0s debuff script

--- Fantasy Riot/Assets/Scripts/Enemies/Debuffs.cs
+++ Fantasy Riot/Assets/Scripts/Enemies/Debuffs.cs
@@ -8,9 +8,11 @@ public class Debuffs : MonoBehaviour
     public float SlowInput;
     private bool stunned = false;
     public bool ranged;
+    public bool assassin;
     public Rigidbody2D rb;
     public Enemy enemySpeed;
     public RangedEnemy rangedSpeed;
+    public EnemyAssassin assassinSpeed;
     
     void Start()
     {
@@ -28,8 +30,18 @@ public class Debuffs : MonoBehaviour
                 Debug.Log("Getting slowed");
             }
         }
-        
-        else if (!ranged)
+
+        if (assassin == true)
+        {
+            if (slowed == false)
+            {
+                assassinSpeed.speed = assassinSpeed.speed - SlowInput;
+                slowed = true;
+                Debug.Log("Getting slowed");
+            }
+        }
+
+        else
         {
             if (slowed == false)
             {
@@ -46,16 +58,26 @@ public class Debuffs : MonoBehaviour
         {
             if (slowed == true)
             {
-                rangedSpeed.speed = 2f;
+                rangedSpeed.speed = rangedSpeed.initialSpeed;
                 slowed = false;
             }
         }
 
-        else if (!ranged)
+        if (assassin == true)
         {
             if (slowed == true)
             {
-                enemySpeed.speed = 2f;
+                assassinSpeed.speed = assassinSpeed.initialSpeed;
+                slowed = false;
+                Debug.Log("Getting slowed");
+            }
+        }
+
+        else
+        {
+            if (slowed == true)
+            {
+                enemySpeed.speed = enemySpeed.initialSpeed;
                 slowed = false;
             }
         }
@@ -85,7 +107,15 @@ public class Debuffs : MonoBehaviour
             }
         }
 
-        else if(!ranged)
+        if (assassin)
+        {
+            if (stunned == false)
+            {
+                assassinSpeed.speed = 0;
+            }
+        }
+        
+        else 
         {
             if (stunned == false)
             {
@@ -101,13 +131,19 @@ public class Debuffs : MonoBehaviour
 
         if(ranged)
         {
-            rangedSpeed.speed = 2f;
+            rangedSpeed.speed = rangedSpeed.initialSpeed;
+            stunned = false;
+        }
+
+        if (assassin)
+        {
+            assassinSpeed.speed = assassinSpeed.initialSpeed;
             stunned = false;
         }
 
         else if(!ranged)
         {
-            enemySpeed.speed = 2f;
+            enemySpeed.speed = enemySpeed.initialSpeed; ;
             stunned = false;
         }
     }

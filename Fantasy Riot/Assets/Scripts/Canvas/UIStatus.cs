f4d21719bbded9cb918897d534251c8d8ee00bf7
zBeppe123 <72344861+zBeppe123@users.noreply.github.com>
2021-01-15 20:14:39 +0100

    Changed Font in Intro and Menu

--- Fantasy Riot/Assets/Scripts/Canvas/UIStatus.cs
+++ Fantasy Riot/Assets/Scripts/Canvas/UIStatus.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 public class UIStatus : MonoBehaviour
 {
     public PlayerStatus playerStatus;
 
-    public Text nameText;
-    public Text levelText, hpText, attackText;
-    public Text nextLevelGems;
+    public TMP_Text nameText;
+    public TMP_Text levelText, hpText, attackText;
+    public TMP_Text nextLevelGems;
 
     // Start is called before the first frame update
     void Start()

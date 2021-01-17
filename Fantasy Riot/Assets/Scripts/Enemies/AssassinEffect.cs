35477c8cf20971fe079aec5a64e314fb01e5f916
Frank KH <72342418+FrankGaudin@users.noreply.github.com>
2021-01-17 15:54:17 +0100

    Added death sounds to the enemies

---
+++ Fantasy Riot/Assets/Scripts/Enemies/AssassinEffect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssassinEffect : MonoBehaviour
+{
+    public AudioSource death;
+    public EnemyAssassin enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!enemy.isAlive)
+        {
+            if (!death.isPlaying)
+                death.Play();
+        }
+        else
+        {
+            death.Stop();
+        }
+    }
+}
---
+++ Fantasy Riot/Assets/Scripts/Enemies/EnemyEffect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyEffect : MonoBehaviour
+{
+    public AudioSource death;
+    public Enemy enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!enemy.isAlive)
+        {
+            if (!death.isPlaying)
+                death.Play();
+        }
+        else
+        {
+            death.Stop();
+        }
+    }
+}

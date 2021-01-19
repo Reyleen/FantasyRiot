08e0b3d9391b72873d05f177e9e119cefe4f9caf
Frank KH <72342418+FrankGaudin@users.noreply.github.com>
2021-01-19 16:33:01 +0100

    Update Logo.cs

--- Fantasy Riot/Assets/Logo.cs
+++ Fantasy Riot/Assets/Logo.cs
@@ -17,7 +17,7 @@ public class Logo : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
-        if(timer>5f)
+        if(timer>4.5f)
         {
             SceneManager.LoadScene(1);
         }

7543ba3baf87c9a6745f85b7f3e66377262ed96c
zBeppe123 <72344861+zBeppe123@users.noreply.github.com>
2021-01-20 21:17:09 +0100

    little change

--- Fantasy Riot/Assets/SetPrefabs.cs
+++ Fantasy Riot/Assets/SetPrefabs.cs
@@ -12,25 +12,29 @@ public class SetPrefabs : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        _player = GameObject.Find("PlayerThings/Player").GetComponent<Player>();
-        arc.currentHp = _player.PlayerData.HPA;
-        arc.playerLevel = _player.PlayerData.lvlA;
-        arc.maxHp = _player.PlayerData.HPA;
-        arc.attack = _player.PlayerData.atkA;
+        if (!PlayerPrefs.HasKey("SetPrefab"))
+        {
+            _player = GameObject.Find("PlayerThings/Player").GetComponent<Player>();
+            arc.currentHp = _player.PlayerData.HPA;
+            arc.playerLevel = _player.PlayerData.lvlA;
+            arc.maxHp = _player.PlayerData.HPA;
+            arc.attack = _player.PlayerData.atkA;
 
-        fig.currentHp = _player.PlayerData.HPF;
-        fig.playerLevel = _player.PlayerData.lvlF;
-        fig.maxHp = _player.PlayerData.HPF;
-        fig.attack = _player.PlayerData.atkF;
+            fig.currentHp = _player.PlayerData.HPF;
+            fig.playerLevel = _player.PlayerData.lvlF;
+            fig.maxHp = _player.PlayerData.HPF;
+            fig.attack = _player.PlayerData.atkF;
 
-        mag.currentHp = _player.PlayerData.HPM;
-        mag.playerLevel = _player.PlayerData.lvlM;
-        mag.maxHp = _player.PlayerData.HPM;
-        mag.attack = _player.PlayerData.atkM;
+            mag.currentHp = _player.PlayerData.HPM;
+            mag.playerLevel = _player.PlayerData.lvlM;
+            mag.maxHp = _player.PlayerData.HPM;
+            mag.attack = _player.PlayerData.atkM;
 
-        lan.currentHp = _player.PlayerData.HPL;
-        lan.playerLevel = _player.PlayerData.lvlL;
-        lan.maxHp = _player.PlayerData.HPL;
-        lan.attack = _player.PlayerData.atkL;
+            lan.currentHp = _player.PlayerData.HPL;
+            lan.playerLevel = _player.PlayerData.lvlL;
+            lan.maxHp = _player.PlayerData.HPL;
+            lan.attack = _player.PlayerData.atkL;
+            PlayerPrefs.SetInt("SetPrefab", 1);
+        }
     }
 }

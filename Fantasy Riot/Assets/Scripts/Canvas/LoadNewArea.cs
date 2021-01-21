c65cfd4e12dd5fd6c3316dfd052218e4e5e2ba0f
Reyleen <72262745+Reyleen@users.noreply.github.com>
2021-01-21 13:54:55 +0100

    Update LoadNewArea.cs

--- Fantasy Riot/Assets/Scripts/Canvas/LoadNewArea.cs
+++ Fantasy Riot/Assets/Scripts/Canvas/LoadNewArea.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class LoadNewArea : MonoBehaviour
 {
     public LoadLevel lvl;
+    public GameObject tutUi;
     
     // Start is called before the first frame update
     void Start()
@@ -23,6 +24,7 @@ public class LoadNewArea : MonoBehaviour
         
         if(other.gameObject.name == "Player")
         {
+            tutUi.SetActive(false);
             lvl.LoadScreen("CaveTutorial");
         }
     

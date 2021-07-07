dc6910bd1e20a3443eede1098747dd279b9f4262
Frank KH <72342418+FrankGaudin@users.noreply.github.com>
2021-07-07 16:31:28 +0200

    Added dungeon and intro maps

---
+++ Fantasy Riot/Assets/SuperTiled2Unity/My maps/Colision.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="Colision" tilewidth="16" tileheight="16" tilecount="1" columns="1">
+ <image source="Colision.png" width="16" height="16"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0222222" y="-0.0222222" width="15.9556" height="16.0667"/>
+  </objectgroup>
+ </tile>
+</tileset>

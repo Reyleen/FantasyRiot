442e7f9a6f5ab70166c700268d5e17022312bcce
Frank KH <72342418+FrankGaudin@users.noreply.github.com>
2021-07-09 12:51:27 +0200

    Added CityMap

---
+++ Fantasy Riot/Assets/SuperTiled2Unity/My maps/Cave.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="Cave" tilewidth="16" tileheight="16" tilecount="448" columns="16">
+ <image source="Cave.png" width="256" height="448"/>
+</tileset>
